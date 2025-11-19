8a6e4476fbc194da546ab3aedcac2f307b352503
mikejigglypuff <k24555569@gmail.com>
2025-11-19 21:53:47 +0900

    [feat] connect TitleService to GameController

--- src/main/java/com/terraria_item_perf_comp/controllers/GameController.java
+++ src/main/java/com/terraria_item_perf_comp/controllers/GameController.java
@@ -1,34 +1,55 @@
 package com.terraria_item_perf_comp.controllers;
 
+import java.util.*;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.http.ResponseEntity;
+
 import com.terraria_item_perf_comp.DTO.responses.GameTitleResDto;
 import com.terraria_item_perf_comp.models.Title;
+
+import lombok.RequiredArgsConstructor;
+
 import com.terraria_item_perf_comp.DTO.responses.ItemCategoryResDto;
 import com.terraria_item_perf_comp.DTO.responses.GameStartResDto;
 import com.terraria_item_perf_comp.models.Progression;
 import com.terraria_item_perf_comp.models.Item;
 import com.terraria_item_perf_comp.DTO.requests.GameStartReqDto;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+
 import com.terraria_item_perf_comp.models.ItemCategory;
 import com.terraria_item_perf_comp.DTO.requests.CompChooseReqDto;
 import com.terraria_item_perf_comp.DTO.requests.BalanceChooseReqDto;
 import com.terraria_item_perf_comp.DTO.responses.BalanceChooseResDto;
+
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.terraria_item_perf_comp.service.ItemStatService;
+import com.terraria_item_perf_comp.service.ItemService;
+import com.terraria_item_perf_comp.service.ItemCategoryService;
+import com.terraria_item_perf_comp.service.ItemCompService;
+import com.terraria_item_perf_comp.service.TitleService;
 
 @RestController
 @RequestMapping("/api/game")
+@RequiredArgsConstructor
 public class GameController {
+  private final TitleService titleService;
+  private final ItemCategoryService itemCategoryService;
+  private final ItemService itemService;
+  private final ItemCompService itemCompService;
+  private final ItemStatService itemStatService;
+
   @GetMapping("/titles")
   public ResponseEntity<GameTitleResDto> getGameTitles() {
-    // TODO: Implement get game titles logic
-    return ResponseEntity.ok(new GameTitleResDto("success", new Title[0]));
+    return ResponseEntity.ok(new GameTitleResDto("success", titleService.getAllTitles()));
   }
 
-  @GetMapping("/titles/${titleId}/categories")
+  @GetMapping("/titles/{titleId}/categories")
   public ResponseEntity<ItemCategoryResDto> getGameCategories(@PathVariable int titleId) {
     // TODO: Implement get game categories logic
     return ResponseEntity.ok(new ItemCategoryResDto("success", new ItemCategory[0]));
